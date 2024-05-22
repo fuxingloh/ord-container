0a8052b7c42559e4f2e9f11f4ee4b6653d04e5f0
Fuxing Loh <git@fuxing.dev>
2024-05-22 11:07:24 +0800

    add base ord docker image

---
+++ packages/karfia-ord/bitcoind-regtest.test.ts
@@ -0,0 +1,38 @@
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
+import { KarfiaContainer, KarfiaTestcontainers } from 'karfia-testcontainers';
+
+import definition from './bitcoind-regtest.json';
+
+let testcontainers: KarfiaTestcontainers;
+
+beforeAll(async () => {
+  testcontainers = await KarfiaTestcontainers.start(definition);
+});
+
+afterAll(async () => {
+  await testcontainers.stop();
+});
+
+describe('bitcoind', () => {
+  let bitcoind: KarfiaContainer;
+
+  beforeAll(() => {
+    bitcoind = testcontainers.getContainer('bitcoind');
+  });
+
+  it('should rpc getblockchaininfo', async () => {
+    const response = await bitcoind.rpc({
+      method: 'getblockchaininfo',
+    });
+
+    expect(response.status).toStrictEqual(200);
+
+    expect(await response.json()).toMatchObject({
+      result: {
+        bestblockhash: '0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206',
+        chain: 'regtest',
+        blocks: 0,
+      },
+    });
+  });
+});
